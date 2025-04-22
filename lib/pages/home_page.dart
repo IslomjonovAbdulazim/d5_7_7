35b6e22834d78353047c2f65af0d0a0f1772d0c1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-22 17:26:46 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +18,20 @@ class _HomePageState extends State<HomePage> {
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Center(
-            child: Column(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  controller: textController,
+                ),
+                SizedBox(height: 20),
+                CupertinoButton(
+                  color: Colors.yellow,
+                  onPressed: () {},
+                  child: Center(child: Text("Save")),
+                ),
+              ],
+            ),
           ),
         ),
       ),
